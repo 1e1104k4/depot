e06656681d6828c4f4d27de47d13250663ce3f08
leila <143362178+1e1104k4@users.noreply.github.com>
2024-12-12 17:35:07 -0600

    feat: add price to line item model

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -4,4 +4,13 @@ class CartTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "add product method" do
+    cart = Cart.create
+    product = products(:ruby)
+    line_item = cart.add_product(product)
+    assert line_item.product.id == product.id
+    assert line_item.price == product.price
+  end
+
 end
