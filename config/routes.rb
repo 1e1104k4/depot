84c8b919f55e2e5486ba158e885cf7e252945550
leila <143362178+1e1104k4@users.noreply.github.com>
2025-01-12 18:43:01 -0600

    feat: selecting locales

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,14 @@ Rails.application.routes.draw do
   get 'sessions/create'
   get 'sessions/destroy'
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index'
+  end
+
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
